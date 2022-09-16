e8063d0544bb389db4320e0d097e5d192911eb76
Toye <ogundepo@gmail.com>
2022-09-16 00:39:42 -0500

    start cart modal functionality.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './Navigation';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,14 +7,27 @@ import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';
 import Cart from "./Cart";
 
 function Home(props) {
     console.log('home props', props)
     console.log("test")
+
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    function handleClick() {
+        console.log('cart was clicked')
+    }
+
     return(
         <>
-            <Navigation />
+            <Navigation clickCart={handleShow} />
             <Container fluid>
             {/* <Cart /> */}
                 <Row>
@@ -179,6 +192,25 @@ function Home(props) {
                     </Col>
                 </Row>
             </Container>
+
+            {/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button> */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </>
     )
 }
